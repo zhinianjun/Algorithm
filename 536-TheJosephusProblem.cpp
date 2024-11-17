de9eed365a845f03225bd9a7c7d03f92d7ea2cbf
zhinianjun <3085934806@qq.com>
2024-11-17 16:33:59 +0800

    Josephus

--- 536-TheJosephusProblem.cpp
+++ 536-TheJosephusProblem.cpp
@@ -2,22 +2,29 @@
 #include <vector>
 
 using namespace std;
+int cir(int n,int m)
+{
+	int p=0;
+	for(int i=2;i<=n;i++)
+	{
+		p=(p+m)%i;
+	}
+	return p+1;
+}
 
 int main()
 {
     int n;
     cin >> n;
     vector<int> list;
-    for(int i = 1; i <= n; i++)
-        list.push_back(i);
-
-    vector<int>::iterator a = list.begin();
-    while(list.size() != 1){
-        a++;
-        if(a == list.begin())
-            a = list.begin();
-        list.erase(a);
-    }
+    
+    for(int i = 0; i < n; i++)
+        list.push_back(i+1);
+    int index = 0;
+     while (list.size() > 1) {
+        index = (index + 1) % list.size();         
+        list.erase(list.begin() + index); 
+     }
     cout << list[0] << endl;
     return 0;
 }
